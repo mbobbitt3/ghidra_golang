c62356694800e971b811d39246e6b958e931ff14
matt <mattbobitt3@gmail.com>
2024-02-05 19:32:05 -0500

    pclntab search functionality works for stripped PE files compiled with Go 1.21. Simple crackme test sample added as well

---
+++ ghidra_scripts/pclntab_ghidra.py
@@ -0,0 +1,38 @@
+from ghidra.program.model.symbol.SourceType import *
+from ghidra.program.model.mem import *
+
+def locate_pclntab_pe():
+	pclntab_magic = ['\xfb\xff\xff\xff\x00\x00',
+		'\xfa\xff\xff\xff\x00\x00',
+		'\xf0\xff\xff\xff\x00\x00',
+		'\xf1\xff\xff\xff\x00\x00',
+		]
+	prog = getCurrentProgram()
+	sections = prog.getMemory().getBlocks()
+	for sect in sections:
+		if sect.getName() == ".rdata":
+			rdata_start = sect.getStart()
+			rdata_size = sect.getSize()
+
+	for magic in pclntab_magic:
+		pclntab = rdata_start
+		while pclntab != None:
+			pclntab = findBytes(pclntab.add(1), magic)
+			if pclntab == None:
+				continue
+			if pclntab_check(pclntab):
+				print("pclntab located")
+				print(pclntab)
+				return pclntab
+	return pclntab
+
+def pclntab_check(addr):
+	pc_arch_byte = getByte(addr.add(6)) #at offset 6 of the pclntab this byte tells architecture (1 for x86 4 for arm)
+	pc_ptr_sz = getByte(addr.add(7)) #at offset 8 this byte determiens pointer size and shuld be 4 or 8
+	
+	if(pc_arch_byte != 1 and pc_arch_byte != 2 and pc_arch_byte != 4) or (pc_ptr_sz != 4 and pc_ptr_sz != 8):
+		return False
+	
+	return True
+
+locate_pclntab_pe()
